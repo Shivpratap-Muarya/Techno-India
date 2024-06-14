90fedc2104edd632a4d2dee4c1b983c90c1df933
Shivpratap-Muarya <72683697+Shivpratap-Muarya@users.noreply.github.com>
2024-06-14 21:36:18 +0530

    Update script.js

--- script.js
+++ script.js
@@ -36,14 +36,14 @@ $(document).ready(function() {
 
   // typing text animation script
   var typed = new Typed(".typing", {
-    strings: ["YouTuber", "Developer", "Blogger", "Designer", "Student"],
+    strings: ["YouTuber", "Developer", "Blogger", "JEE Aspirant", "Student"],
     typeSpeed: 100,
     backSpeed: 60,
     loop: true
   });
 
   var typed = new Typed(".typing-2", {
-    strings: ["YouTuber", "Developer", "Blogger", "Designer", "Student"],
+    strings: ["YouTuber", "Developer", "Blogger", "JEE Aspirant", "Student"],
     typeSpeed: 100,
     backSpeed: 60,
     loop: true
